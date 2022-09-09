35f3a56618d65ab8e87cffa38d4aa70fe6863d5c
M6Irinel <momoloacairinel666@gmail.com>
2022-09-09 15:46:08 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+
+  methods: {
+    requestApi () {
+      axios.get( 'https://developers.themoviedb.org/3/search/movie?api_key=ee490867376da94e7a75a9c4520d56a1&query=future' )
+        .then( r => console.log( r ) );
+    }
+  },
+
+  beforeMount () {
+    this.requestApi();
+  }
+}
+</script>
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+export default Vue.observable( {
+    baseURL: 'https://developers.themoviedb.org/3',
+    api_key: 'ee490867376da94e7a75a9c4520d56a1',
+    resultAPI: []
+} );
